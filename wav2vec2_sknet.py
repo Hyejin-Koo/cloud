d67abd2dd22d2686e26cda67ee73fc56247c3066
koo <52940511+Hyejin-Koo@users.noreply.github.com>
2021-07-30 03:16:07 +0900

    Update wav2vec2_sknet.py
    SKconv from https://github.com/ResearchingDexter/SKNet_pytorch/blob/master/SKNet.py

--- wav2vec2_sknet.py
+++ wav2vec2_sknet.py
@@ -28,6 +28,8 @@ from fairseq.modules import (
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
 from fairseq.utils import buffered_arange
 
+from functools import reduce
+
 
 EXTRACTOR_MODE_CHOICES = ChoiceEnum(["default", "layer_norm"])
 MASKING_DISTRIBUTION_CHOICES = ChoiceEnum(["static", "uniform", "normal", "poisson"])
@@ -623,7 +625,8 @@ class ConvFeatureExtractionModel(nn.Module):
             conv_bias=False,
         ):
             def make_conv():
-                conv = nn.Conv1d(n_in, n_out, k, stride=stride, bias=conv_bias)
+                #conv = nn.Conv1d(n_in, n_out, k, stride=stride, bias=conv_bias)
+                conv = SKConv(n_in, n_out,stride=stride)
                 nn.init.kaiming_normal_(conv.weight)
                 return conv
 
@@ -682,6 +685,46 @@ class ConvFeatureExtractionModel(nn.Module):
         return x
 
 
+class SKConv(nn.Module):
+    def __init__(self,in_channels,out_channels,stride=1,M=2,r=16,L=32):
+        super(SKConv,self).__init__()
+        d=max(in_channels//r,L)
+        self.M=M
+        self.out_channels=out_channels
+        self.conv=nn.ModuleList()
+        for i in range(M):
+            self.conv.append(nn.Sequential(nn.Conv2d(in_channels,out_channels,3,stride,padding=1+i,dilation=1+i,groups=32,bias=False),
+                                           nn.BatchNorm2d(out_channels),
+                                           nn.ReLU(inplace=True)))
+        self.global_pool=nn.AdaptiveAvgPool2d(1)
+        self.fc1=nn.Sequential(nn.Conv2d(out_channels,d,1,bias=False),
+                               nn.BatchNorm2d(d),
+                               nn.ReLU(inplace=True))
+        self.fc2=nn.Conv2d(d,out_channels*M,1,1,bias=False)
+        self.softmax=nn.Softmax(dim=1)
+    def forward(self, input):
+        batch_size=input.size(0)
+        output=[]
+        #the part of split
+        for i,conv in enumerate(self.conv):
+            #print(i,conv(input).size())
+            output.append(conv(input))
+        #the part of fusion
+        U=reduce(lambda x,y:x+y,output)
+        s=self.global_pool(U)
+        z=self.fc1(s)
+        a_b=self.fc2(z)
+        a_b=a_b.reshape(batch_size,self.M,self.out_channels,-1)
+        a_b=self.softmax(a_b)
+        #the part of selection
+        a_b=list(a_b.chunk(self.M,dim=1))#split to a and b
+        a_b=list(map(lambda x:x.reshape(batch_size,self.out_channels,1,1),a_b))
+        V=list(map(lambda x,y:x*y,output,a_b))
+        V=reduce(lambda x,y:x+y,V)
+        return V
+    
+    
+    
 class TransformerEncoder(nn.Module):
     def __init__(self, args):
         super().__init__()
