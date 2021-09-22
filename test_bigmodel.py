1760758c6a0ab39688bd9985a4df1f4979ce990f
koo <52940511+Hyejin-Koo@users.noreply.github.com>
2021-09-22 20:42:35 +0900

    Create test_bigmodel.py
    why haven't I made this file yet

---
+++ test_bigmodel.py
@@ -0,0 +1,13 @@
+import fairseq #don't required
+import torch
+
+from omegaconf import OmegaConf
+from desed_task.nnet.WRNN_2_ import WRNN
+
+import pdb
+
+model = torch.load('/home1/irteam/users/koo/trained2.pt')
+big_cfg = OmegaConf.create(vars(vars(model['args'])['w2v_args']))
+
+sed = WRNN(big_cfg)
+pdb.set_trace()
